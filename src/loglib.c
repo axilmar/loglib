055c5f883a861316426af543cd9cb22b69b07386
axilmar <axilmar@gmail.com>
2021-12-24 12:09:52 +0200

    Added milliseconds to logging.

--- src/loglib.c
+++ src/loglib.c
@@ -90,14 +90,53 @@ static void strout(OUTPUT_DATA* data, const char* format, ...) {
 }
 
 
+#ifdef _WIN32
+//taken from stackoverflow
+int gettimeofday(struct timeval* tp, struct timezone* tz)
+{
+    // Note: some broken versions only have 8 trailing zero's, the correct epoch has 9 trailing zero's
+    // This magic number is the number of 100 nanosecond intervals since January 1, 1601 (UTC)
+    // until 00:00:00 January 1, 1970 
+    static const uint64_t EPOCH = ((uint64_t)116444736000000000ULL);
+
+    SYSTEMTIME  system_time;
+    FILETIME    file_time;
+    uint64_t    time;
+
+    GetSystemTime(&system_time);
+    SystemTimeToFileTime(&system_time, &file_time);
+    time = ((uint64_t)file_time.dwLowDateTime);
+    time += ((uint64_t)file_time.dwHighDateTime) << 32;
+
+    tp->tv_sec = (long)((time - EPOCH) / 10000000L);
+    tp->tv_usec = (long)(system_time.wMilliseconds * 1000);
+    return 0;
+}
+#endif
+
+
 //get datetime string
 static void get_datetime_str(OUTPUT_DATA* data) {
-    time_t timeval = time(NULL);
-    struct tm tms;
-    localtime_s(&tms, &timeval);
-    int written = (int)strftime(data->out, data->len, "%Y-%m-%e %H:%M:%S", &tms);
+    //get time of day
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+
+    //convert seconds to a tm structure
+    time_t sec = (time_t)tv.tv_sec;
+    struct tm tmv;
+    #ifdef _WIN32
+    localtime_s(&tmv, &sec);
+    #else
+    localtime_s(&sec, &tmv);
+    #endif
+
+    //write the date/time part
+    int written = (int)strftime(data->out, data->len, "%Y-%m-%e %H:%M:%S", &tmv);
     data->out += written;
     data->len -= written;
+
+    //write the milliseconds part
+    strout(data, ".%03i", tv.tv_usec / 1000);
 }
 
 
